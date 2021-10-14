39dabb0fd83b706a81296445520cd0b04fc7b131
MasatoMakino <unagiinu.g@gmail.com>
2021-10-14 20:42:22 +0900

    add : private member

--- src/SubClass.ts
+++ src/SubClass.ts
@@ -1,11 +1,12 @@
 import {MainClass} from "./MainClass";
 
 export class SubClass extends MainClass{
+    private memberValue:number = 1.0;
     add(val:number):number{
-        return super.add(val) + 1;
+        return super.add(val) + this.memberValue;
     }
     sub( val:number):number{
-        return super.sub(val) - 1;
+        return super.sub(val) - this.memberValue;
     }
 
 }
