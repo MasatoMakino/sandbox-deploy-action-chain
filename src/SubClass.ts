fc5ed1a6bc8e2f36a5ecb62ca2e4fd03a451cdf6
MasatoMakino <unagiinu.g@gmail.com>
2021-10-14 21:30:16 +0900

    remove : member

--- src/SubClass.ts
+++ src/SubClass.ts
@@ -2,8 +2,6 @@ import {MainClass} from "./MainClass";
 
 export class SubClass extends MainClass{
     private memberValue:number = 1.0;
-    private member2:string = "member";
-
     add(val:number):number{
         return super.add(val) + this.memberValue;
     }
