23c30533e190e9b434ce057d7dc14a691d7a23d8
MasatoMakino <unagiinu.g@gmail.com>
2021-10-16 11:51:42 +0900

    reformat

--- src/SubClass.ts
+++ src/SubClass.ts
@@ -1,20 +1,20 @@
-import {MainClass} from "./MainClass";
+import { MainClass } from "./MainClass";
 
-export class SubClass extends MainClass{
-    private memberValue:number = 1.0;
-    add(val:number):number{
-        return super.add(val) + this.memberValue;
-    }
-    sub( val:number):number{
-        return super.sub(val) - this.memberValue;
-    }
-    foo(val:number):number{
-        return val;
-    }
-    bar(val:number):number{
-        return val/2;
-    }
-    baz(val:number):number{
-        return val*2;
-    }
+export class SubClass extends MainClass {
+  private memberValue: number = 1.0;
+  add(val: number): number {
+    return super.add(val) + this.memberValue;
+  }
+  sub(val: number): number {
+    return super.sub(val) - this.memberValue;
+  }
+  foo(val: number): number {
+    return val;
+  }
+  bar(val: number): number {
+    return val / 2;
+  }
+  baz(val: number): number {
+    return val * 2;
+  }
 }
